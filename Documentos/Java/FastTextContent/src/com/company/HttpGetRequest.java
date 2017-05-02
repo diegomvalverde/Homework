b468a79ac1bd062755f98ced3177cd39ce8188ad
Diego Méndez Valverde <diegomvalverde@outlook.com>
2017-05-02 15:00:15 -0600

    The Request to Google works, a little part of the interface is done.

---
+++ Documentos/Java/FastTextContent/src/com/company/HttpGetRequest.java
@@ -0,0 +1,50 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+public class HttpGetRequest
+{
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+
+    void getRequest(String tmp) throws Exception {                          // Metodo para realizar un request a Google.
+
+        String url = "http://www.google.com/search?q=" + tmp;
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+
+}
--- Documentos/Java/FastTextContent/src/com/company/Main.java
+++ Documentos/Java/FastTextContent/src/com/company/Main.java
@@ -2,8 +2,16 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    Interface tmp = new Interface();
-	    tmp.showPanel();
+    public static void main(String[] args) throws Exception {
+//	    Interface tmp = new Interface();
+//	    tmp.showPanel();
+        HttpGetRequest http = new HttpGetRequest();
+
+        System.out.println("Testing 1 - Send Http GET request");
+        http.getRequest("Hola");
+
+        System.out.println("\nTesting 2 - Send Http POST request");
+        http.getRequest("Avion");
+
     }
 }
